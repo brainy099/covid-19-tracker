69b5204392eb69d1a416585be1404b4a6c4bac0d
brainy099 <brainy099@gmail.com>
2020-05-05 12:29:13 +0530

    added country filter added bar chart

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 
 const url = 'https://covid19.mathdro.id/api'
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url
+    if(country) 
+        changeableUrl = `${url}/countries/${country}`
+
     try {
-        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(url)
+        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(changeableUrl)
         const modifiedData =  {
             confirmed,
             recovered,
@@ -26,6 +30,17 @@ export const fetchDailyData = async() => {
             date: dailyData.reportDate
         }))
         return modifiedData
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+export const fetchCountries = async () => {
+    try{
+        const {data: { countries }} = await axios.get(`${url}/countries`)
+        
+        return countries.map(country => country.name)
+
     } catch(err) {
         console.log(err)
     }
