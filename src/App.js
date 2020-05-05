0b46c77c8c783536c2c8695fdc41fbcb59c159c4
brainy099 <brainy099@gmail.com@users.noreply.github.com>
2020-05-05 11:23:38 +0530

    added cards and charts

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React from "react"
 
 import { Cards, Chart, CountryPicker } from './components'
 import styles from "./App.module.css"
+import { fetchData } from './api'
 
-const App = () => {
-  return (
-    <div className={styles.container}>
-      <Cards />
-      <CountryPicker />
-      <Chart />
-    </div>
-  );
-};
+class App  extends React.Component {
+  state = {
+    data : {}
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData()
+    this.setState({ data: fetchedData })
+  }
+
+  render() {
+    const { data } = this.state
+    return (
+      <div className={styles.container}>
+        <Cards data={data}/>
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
+}
 
 export default App
